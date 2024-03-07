ab05bc92c7f1e50b6e48430153fa443f6e038fc2
ふぁ <yuki@yuki0311.com>
2024-03-07 17:56:39 +0900

    update cache
    Signed-off-by: ふぁ <yuki@yuki0311.com>

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import {
 	TwitterOpenApi
 } from "twitter-openapi-typescript";
-import { ImageResponse } from "workers-og";
-import { loadGoogleFont } from "./font";
+import { ImageResponse, loadGoogleFont } from "workers-og";
 import getNormalComponent from "./twitter-snap";
 
 export interface Env {
@@ -16,23 +15,40 @@ export default {
 		const pathArray = url.pathname.split('/');
 		const id = pathArray[1];
 
-		TwitterOpenApi.fetchApi = fetch.bind(globalThis);
-		const api = await new TwitterOpenApi().getGuestClient();
-		const tweet = await api.getDefaultApi().getTweetResultByRestId({
-			tweetId: id,
-		});
-		return new ImageResponse(getNormalComponent({ data: tweet.data!, video: false, width: 600 }), {
-			format: "png",
-			width: 600,
-			height: "auto",
-			fonts: [
-				{
-					name: "Noto Sans JP",
-					data: await loadGoogleFont({ family: "Noto Sans JP", weight: 600 }),
-					weight: 500,
-					style: "normal",
-				},
-			],
-		});
+		const cache = caches.default;
+		const cacheKey = request.url;
+		let res = await cache.match(cacheKey);
+
+		if (res) {
+			return res;
+		} else {
+			TwitterOpenApi.fetchApi = fetch.bind(globalThis);
+			const api = await new TwitterOpenApi().getGuestClient();
+			const tweet = await api.getDefaultApi().getTweetResultByRestId({
+				tweetId: id,
+			});
+			const res = new ImageResponse(getNormalComponent({ data: tweet.data!, video: false, width: 600 }), {
+				format: "png",
+				width: 600,
+				emoji: "twemoji",
+				fonts: [
+					{
+						name: "Noto Sans JP",
+						data: await loadGoogleFont({ family: "Noto Sans JP", weight: 400 }),
+						weight: 400,
+						style: "normal"
+					},
+					{
+						name: "Noto Sans JP",
+						data: await loadGoogleFont({ family: "Noto Sans JP", weight: 700 }),
+						weight: 700,
+						style: "normal"
+					},
+				],
+			});
+			await cache.put(cacheKey, res.clone());
+			res.headers.append("Cache-Control", "s-maxage=3600");
+			return res;
+		}
 	},
 };
